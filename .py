e64f8c3c7091833daf7dacb29681a178387f101d
Djmattkoma <55582553+Djmattkoma@users.noreply.github.com>
2020-10-01 10:10:06 +0200

    Create .py

---
+++ .py
@@ -0,0 +1,41 @@
+#inport random
+
+def negoszz():
+    ossz =0
+    for _ in range(10):
+        be = int(input("Kérek egy számot:"))
+        if be <0:
+            ossz +=beszam
+    print("negatívok összege:", ossz)
+
+#hátul tesztekés nincs
+def ellnemmin():
+    global be
+    while  not(be<0 and szam % 3 == 0):
+        be = int(input("Kérek egy számot:"))
+
+
+#5 szám összege
+def otosz():
+    global osseg
+    for _ in range(5):
+        var = beker in (input("5db számot kérek:"))
+        osseg += beker
+
+#5 + min
+def minimum():
+    min = 0
+    for _ in range(5):
+         beker = int (input("5db számot kérek:"))
+        if beker > min:
+            print("legkissebb", min)
+
+def minimum2():
+    min2 = 0
+    for _ in range(5):
+        be
+
+#negoszz()
+#ellnemmin()
+#otosz()
+minimum()
