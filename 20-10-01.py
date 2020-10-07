d4a77e3684cb8e74101d544ed3e90faa0c4d8eb3
Djmattkoma <55582553+Djmattkoma@users.noreply.github.com>
2020-10-07 11:57:50 +0200

    2.ora

--- 20-10-01.py
+++ 20-10-01.py
@@ -1,41 +1,14 @@
-#inport random
-
-def negoszz():
-    ossz =0
-    for _ in range(10):
-        be = int(input("Kérek egy számot:"))
-        if be <0:
-            ossz +=beszam
-    print("negatívok összege:", ossz)
-
-#hátul tesztekés nincs
-def ellnemmin():
-    global be
-    while  not(be<0 and szam % 3 == 0):
-        be = int(input("Kérek egy számot:"))
+def F2():
+    szv=""
+    for i in range(5):
+        szv+=input("Ide kell: ")+", "
+    print(szv)
 
 
-#5 szám összege
-def otosz():
-    global osseg
-    for _ in range(5):
-        var = beker in (input("5db számot kérek:"))
-        osseg += beker
+def F3():
+    num = 0
+    for i in range(5):
+        num+= input("számok: ")
+        if  num %  2:
+            print(num)
 
-#5 + min
-def minimum():
-    min = 0
-    for _ in range(5):
-         beker = int (input("5db számot kérek:"))
-        if beker > min:
-            print("legkissebb", min)
-
-def minimum2():
-    min2 = 0
-    for _ in range(5):
-        be
-
-#negoszz()
-#ellnemmin()
-#otosz()
-minimum()
